8cc4f794f28f302e6dae9f33d3ea4157be626052
Anas Firly <anasfirly20@gmail.com>
2023-09-29 18:31:36 +0700

    iterating a slice

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
-	for x <= 5 {
-		fmt.Println("value of x is:", x)
-		x++
+
+	// // WHILE LOOP
+	// x := 0
+	// for x <= 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++
+	// }
+
+	// // FOR LOOP
+	// for i := 0; i <= 5 ; i++ {
+	// 	fmt.Println("value of i is:", i)
+	// }
+
+	// Iterating a slice
+	names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	for i := 0; i < len(names); i++{
+		fmt.Println("NAME ->>", i, names[i])
 	}
 }
