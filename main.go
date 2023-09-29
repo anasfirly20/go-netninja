6f4432c3608d96e58593158662b4c4451c56a1ae
Anas Firly <anasfirly20@gmail.com>
2023-09-29 16:59:25 +0700

    arrays / slices

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// ARRAYS
+	// var ages [3]int = [3]int{35, 40, 45}
+	var ages = [3]int{35, 40, 45}
+	fmt.Println(ages, len(ages))
+	
+	names := [4]string{"Muhammad", "Thomi", "Anas", "Firly"}
+	fmt.Println(names)
+	
+
+	// SLICES
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
+
+	fmt.Println(scores, len(scores))
+}
+
+
