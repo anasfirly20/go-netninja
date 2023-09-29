8a5a874b8ec74bc9980282188c7186faa1d71ade
Anas Firly <anasfirly20@gmail.com>
2023-09-30 05:48:17 +0700

    slice iteration with conditioning

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	age := 45
+	// age := 45
 
-	if age < 30 {
-		fmt.Println("Age is less than 30")
-	} else if age < 20 {
-		fmt.Println("Age is less than 20")
-	} else {
-		fmt.Println("Age is less than 45")
+	// if age < 30 {
+	// 	fmt.Println("Age is less than 30")
+	// } else if age < 20 {
+	// 	fmt.Println("Age is less than 20")
+	// } else {
+	// 	fmt.Println("Age is less than 45")
+	// }
+
+	// 
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("Breaking at pos", index)
+			break
+		}
+
+		fmt.Printf("The value at pos %v is %v \n", index, value)
 	}
 }
