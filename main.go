d203821e2b0d562e86bb6814f9c944335fdb7701
Anas Firly <anasfirly20@gmail.com>
2023-09-30 06:12:35 +0700

    function as argument

--- main.go
+++ main.go
@@ -6,12 +6,23 @@ import "fmt"
 func sayGreeting(n string){
 	fmt.Printf("Good morning %v \n", n)
 }
+
 func sayBye(n string){
 	fmt.Printf("Goodbye %v \n", n)
 }
 
+func cycleNames(n []string, f func(string)){
+	for _, v := range n {
+		f(v)
+	}
+}
+
 func main() {
-	sayGreeting("mario")
-	sayGreeting("luigi")
-	sayBye("mario")
+	// sayGreeting("mario")
+	// sayGreeting("luigi")
+	// sayBye("mario")
+
+	cycleNames([]string{"cloud", "mario", "barret"}, sayGreeting)
+	cycleNames([]string{"cloud", "mario", "barret"}, sayBye)
+	
 }
