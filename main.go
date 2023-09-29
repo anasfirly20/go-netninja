1315708ae7f9eabeed715e2521b9ffe574fd562d
Anas Firly <anasfirly20@gmail.com>
2023-09-30 06:06:45 +0700

    functions

--- main.go
+++ main.go
@@ -2,30 +2,16 @@ package main
 
 import "fmt"
 
+
+func sayGreeting(n string){
+	fmt.Printf("Good morning %v \n", n)
+}
+func sayBye(n string){
+	fmt.Printf("Goodbye %v \n", n)
+}
+
 func main() {
-	// age := 45
-
-	// if age < 30 {
-	// 	fmt.Println("Age is less than 30")
-	// } else if age < 20 {
-	// 	fmt.Println("Age is less than 20")
-	// } else {
-	// 	fmt.Println("Age is less than 45")
-	// }
-
-	// 
-	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
-
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("Continuing at pos", index)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("Breaking at pos", index)
-			break
-		}
-
-		fmt.Printf("The value at pos %v is %v \n", index, value)
-	}
+	sayGreeting("mario")
+	sayGreeting("luigi")
+	sayBye("mario")
 }
