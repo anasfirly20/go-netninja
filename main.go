827b6fad2171f1300efe18cb5aefa34afb3bd43b
Anas Firly <anasfirly20@gmail.com>
2023-09-30 18:31:03 +0700

    package scope

--- main.go
+++ main.go
@@ -1,34 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	fmt.Println("N >",n)
-
-	s := strings.ToUpper(n)
-	fmt.Println("S >",s)
-	
-	names := strings.Split(s, " ")
-	fmt.Println("Names >",names)
-
-	var initials []string
-	for _, v := range names {
-		fmt.Println("CHECK>>>", v[:1])
-		initials = append(initials, v[:1])
-	}
-	
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-} 
+var score = 99.5
 
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
 	
+	sayHello("maria")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScore()
 }
